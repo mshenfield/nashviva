b363a4a4201241facddc758d7796a6ff4fd15500
Colby DeHart <colbydehart@gmail.com>
2015-02-22 16:38:27 -0600

    added markers for police, parks, and fire and toggles

--- js/home.js
+++ js/home.js
@@ -1,5 +1,5 @@
 (function(){
-angular.module('home', ['ngRoute'])
+    angular.module('home', ['ngRoute', 'factories'])
 
 .config(function($routeProvider){
     $routeProvider
@@ -9,12 +9,37 @@ angular.module('home', ['ngRoute'])
         controllerAs: 'vm'
     });
 })
-.controller('HomeController', function(){
-    var map = L.map('map').setView([36.165818, -86.784245], 13);
-    L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
-        subdomains: ['otile1','otile2','otile3','otile4']
-    }).addTo( map );
+.controller('HomeController', function(PublicInfoFactory, MapFactory){
+    //Initialize the Leaflet.js map. 
+    var map = MapFactory,
+        info = PublicInfoFactory,
+        vm = this;
+
+    var leaf = map.initMap();
+
+    vm.toggle = function(type){
+        if(vm[type]){
+            vm[type].forEach(function(point){
+              var op = point.options.opacity > 0 ? 0 : 1;
+              point.setOpacity(op);
+            });
+        }
+        else{
+            info[type]()
+            .success(function (data) {
+                var markPoints = data.map(function(el){
+                    return [el.location.reverse(), {title:el.address}];
+                });
+                vm[type] = map.PlacePoints(markPoints, leaf);
+            })
+            .error(function (data) {
+                console.error(data);
+            });
+        }
+    };
+
+    
+    vm.toggle('fire');
 });
 
 })();
